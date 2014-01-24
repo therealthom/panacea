eaeb8f9b3263176b57d43e68d975f904b1e6c1fa
Tomás Moreno <the.real.thom@gmail.com>
2014-01-24 13:20:46 -0600

    log

--- src/main/java/com/web/panacea/controller/HomeController.java
+++ src/main/java/com/web/panacea/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.web.panacea.controller;
 
+import java.util.logging.Level;
 import javax.servlet.http.HttpSession;
+import java.util.logging.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,10 +12,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/home")
 public class HomeController {
             
+    private static final Logger logger = Logger.getLogger("HomeController");
+    
     @RequestMapping(value = "/dashboard", method = RequestMethod.GET)
     public String dashboard(HttpSession session) {
         //return "redirect:/home/user";
         System.out.println(">>>" + session.getAttribute("login"));
+        logger.log(Level.INFO, "!!! {0}", session.getAttribute("login"));
         return "dashboard";
     }
 }
