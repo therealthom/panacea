01bd1da3535e525326beca0c32d209b6cbd2794b
Tomás Moreno <the.real.thom@gmail.com>
2014-01-28 13:51:17 -0600

    este si jala en jboss 6.0.1

--- src/main/java/com/web/panacea/service/SessionServiceImpl.java
+++ src/main/java/com/web/panacea/service/SessionServiceImpl.java
@@ -1,17 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.web.panacea.service;
 
 import javax.servlet.http.HttpSession;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-/**
- *
- * @author oscar
- */
+@Service
+@Transactional
 public class SessionServiceImpl implements SessionService {
     
     public String getRole(HttpSession session){
